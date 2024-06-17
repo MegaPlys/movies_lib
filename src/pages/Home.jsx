4550d272db2ee2556e53593e6b0274f04e71102b
Júlio Rodrigues <116827694+MegaPlys@users.noreply.github.com>
2024-06-17 20:32:24 -0300

    ML3 - Componetização do Movie Card
    Criado componente "movie card" com o poster, avaliação, titulo etc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY; 
@@ -24,9 +25,16 @@ const Home = () => {
 
     }, [])
 
-    return <div>
-        {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
-    </div>
+    return (
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>carregando...</p>}
+                {topMovies.length > 0 && 
+                topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+            </div>
+        </div>
+    )
 }
 
 export default Home;
